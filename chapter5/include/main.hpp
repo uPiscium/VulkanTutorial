937b895d9aa9dadaa5680a63f2c7583a7da8b3fd
uPiscium <upiscium@gmail.com>
2025-10-17 21:42:34 +0900

    Add Chapter5 codes

---
+++ chapter5/include/main.hpp
@@ -0,0 +1,141 @@
+#pragma once
+
+#include <common.hpp>
+#include <util.hpp>
+
+namespace VulkanTutorial::Chapter4 {
+
+struct Vertex {
+  glm::vec2 pos;
+  glm::vec3 color;
+
+  static VkVertexInputBindingDescription getBindingDescription();
+  static vec<VkVertexInputAttributeDescription> getAttributeDescriptions();
+};
+
+static vec<Vertex> const VERTICES = {{{-0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+                                     {{0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}},
+                                     {{0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}},
+                                     {{-0.5f, 0.5f}, {1.0f, 1.0f, 1.0f}}};
+static vec<u16> const INDICES = {0, 1, 2, 2, 3, 0};
+
+class App {
+private:
+  bool mDebugMode = true;
+
+  SDL_Window *mWindow;
+  VkInstance mInstance = VK_NULL_HANDLE;
+  VkDebugUtilsMessengerEXT mDebugMessenger = VK_NULL_HANDLE;
+  VkSurfaceKHR mSurface = VK_NULL_HANDLE;
+
+  VkPhysicalDevice mPhysicalDevice = VK_NULL_HANDLE;
+  VkDevice mDevice = VK_NULL_HANDLE;
+
+  VkQueue mGraphicsQueue = VK_NULL_HANDLE;
+  VkQueue mPresentQueue = VK_NULL_HANDLE;
+
+  VkSwapchainKHR mSwapchain = VK_NULL_HANDLE;
+  vec<VkImage> mSwapchainImages;
+  VkFormat mSwapchainImageFormat;
+  VkExtent2D mSwapchainExtent;
+  vec<VkImageView> mSwapchainImageViews;
+
+  VkRenderPass mRenderPass = VK_NULL_HANDLE;
+  VkDescriptorSetLayout mDescriptorSetLayout = VK_NULL_HANDLE;
+  VkPipelineLayout mPipelineLayout = VK_NULL_HANDLE;
+  VkPipeline mGraphicsPipeline = VK_NULL_HANDLE;
+  vec<VkFramebuffer> mSwapchainFramebuffers;
+
+  VkCommandPool mCommandPool = VK_NULL_HANDLE;
+  vec<VkCommandBuffer> mCommandBuffers = {};
+
+  VkBuffer mVertexBuffer = VK_NULL_HANDLE;
+  VkDeviceMemory mVertexBufferMemory = VK_NULL_HANDLE;
+  VkBuffer mIndexBuffer = VK_NULL_HANDLE;
+  VkDeviceMemory mIndexBufferMemory = VK_NULL_HANDLE;
+
+  vec<VkSemaphore> mImageAvailableSemaphores = {};
+  vec<VkSemaphore> mRenderFinishedSemaphores = {};
+  vec<VkFence> mInFlightFences = {};
+
+  u32 mCurrentFrame = 0;
+  bool mFramebufferResized = false;
+
+private:
+  void initWindow(int const &width, int const &height, str const &title);
+
+  bool checkValidationLayerSupport();
+  vec<char const *> getRequiredExtensions();
+  void populateDebugMessengerCreateInfo(
+      VkDebugUtilsMessengerCreateInfoEXT &createInfo);
+  void createInstance(str const &appName, u32 const &appVersion);
+
+  VkResult createDebugUtilsMessengerEXT(
+      VkDebugUtilsMessengerCreateInfoEXT const *pCreateInfo,
+      VkAllocationCallbacks const *pAllocator);
+  VkResult
+  destroyDebugUtilsMessengerEXT(VkAllocationCallbacks const *pAllocator);
+  void setupDebugMessenger();
+
+  void createSurface();
+
+  QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
+  bool checkDeviceExtensionSupport(VkPhysicalDevice device);
+  SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device);
+  int rateDevice(VkPhysicalDevice device);
+  void pickPhysicalDevice();
+
+  void createLogicalDevice();
+  void createQueue();
+
+  VkSurfaceFormatKHR
+  chooseSwapSurfaceFormat(vec<VkSurfaceFormatKHR> const &availableFormats);
+  VkPresentModeKHR
+  chooseSwapPresentMode(vec<VkPresentModeKHR> const &availablePresentModes);
+  VkExtent2D chooseSwapExtent(VkSurfaceCapabilitiesKHR const &capabilities);
+  void createSwapchain();
+  void createImageViews();
+
+  VkShaderModule createShaderModule(vec<char> const &code);
+
+  void createCommandPool();
+
+  void createRenderPass();
+  void createDescriptorSetLayout();
+  void createGraphicsPipeline();
+  void createFramebuffers();
+
+  u32 findMemoryType(u32 typeFilter, VkMemoryPropertyFlags properties);
+  void createBuffer(VkDeviceSize size, VkBufferUsageFlags usage,
+                    VkMemoryPropertyFlags properties, VkBuffer &buffer,
+                    VkDeviceMemory &bufferMemory);
+  void copyBuffer(VkBuffer srcBuffer, VkBuffer dstBuffer, VkDeviceSize size);
+
+  void createVertexBuffer();
+  void createIndexBuffer();
+
+  void createCommandBuffers();
+  void recordCommandBuffer(VkCommandBuffer commandBuffer, u32 imageIndex);
+
+  void createSyncObjects();
+
+  void cleanupSwapchain();
+  void recreateSwapchain();
+
+  void initVulkan();
+  bool pollEvents();
+  void drawFrame();
+  void mainLoop();
+  void cleanup();
+
+public:
+  App(int const &width = WINDOW_WIDTH, int const &height = WINDOW_HEIGHT,
+      str const &title = "Vulkan Tutorial", bool debugMode = true);
+  ~App();
+
+  void run();
+};
+
+} // namespace VulkanTutorial::Chapter4
+
+int main();
