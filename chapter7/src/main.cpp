6a59a151202fc5b0061f676e555bb34d2d5020a2
uPiscium <upiscium@gmail.com>
2025-10-22 14:53:25 +0900

    Fix chapter5, chapter6, and chapter7 examples

--- chapter7/src/main.cpp
+++ chapter7/src/main.cpp
@@ -16,7 +16,7 @@ vec<VkVertexInputAttributeDescription> Vertex::getAttributeDescriptions() {
 
   attributeDescriptions[0].binding = 0;
   attributeDescriptions[0].location = 0;
-  attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
+  attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
   attributeDescriptions[0].offset = offsetof(Vertex, pos);
 
   attributeDescriptions[1].binding = 0;
